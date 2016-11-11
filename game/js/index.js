9bfcd3cb248eb47010b8b334f7df4082f5f71e57
Ken <kh@bsdacademy.com>
2016-11-11 14:41:34 +0800

    d

--- game/js/index.js
+++ game/js/index.js
@@ -69,3 +69,42 @@ function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+    function collision($div1, $div2) {
+      var x1 = $div1.offset().left;
+      var y1 = $div1.offset().top;
+      var h1 = $div1.outerHeight(true);
+      var w1 = $div1.outerWidth(true);
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = $div2.offset().left;
+      var y2 = $div2.offset().top;
+      var h2 = $div2.outerHeight(true);
+      var w2 = $div2.outerWidth(true);
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
+        
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        //if it is not colliding, make div2 background green
+
+        return false;
+      } 
+      else {  
+        //if it is colliding, make div2 background yellow
+        $("#enemy1").hide();
+        return true;
+      }
+    }
+
+window.setInterval(function() {
+    $('#result').text(collision($('#bullet'), $('#enemy1')));
+}, 200);
+
+window.setInterval(function() {
+    $('#enemy1').show();
+}, 3000);
+
+//window.setInterval(function() {
+  //  $('#enemy2').show();
+//}, 3000);
+
